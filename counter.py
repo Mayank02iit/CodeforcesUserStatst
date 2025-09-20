465ce8ba957ab0d81c2d73ba454e5cf3b67c10f0
Mayank02iit <ee23b045@smail.iitm.ac.in>
2025-09-20 13:58:30 +0530

    functions added

--- counter.py
+++ counter.py
@@ -7,9 +7,10 @@ count = Counter()
 options = Options()
 options.add_argument("--headless=new")
 options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"')      
-def find(linksFile,InfoFile):
+
+def findData(linksFile,InfoFile,destinationpath):
     driver = webdriver.Chrome(options)
-    with open(linksFile, 'r') as file:
+    with open(destinationpath+linksFile, 'r') as file:
         for line in file:
             link = line.strip()
             driver.get(link)
@@ -20,7 +21,7 @@ def find(linksFile,InfoFile):
             elif len(sideboxes)==1:
                 box = re.split(r'[\s]+', sideboxes[-1].text)
             else :
-                print(sideboxes)
+                pass
             if box:
                 Flag = False
                 for element in box:
@@ -29,8 +30,11 @@ def find(linksFile,InfoFile):
                         Flag = True
                 if not Flag :
                     count[0]+=1
-
-    with open(InfoFile , 'a') as f:
+                    
+    count = sorted(d.items())
+    with open(destinationpath+InfoFile , 'a') as f:
         for key ,value in count.items():
             f.write(f"The number of questions solved whose rating {key} is {value}\n")
+    
+    print(f"Information is successfully written in the file {destinationpath+InfoFile}")
 
--- parser.py
+++ parser.py
@@ -1,30 +1,28 @@
-# options = Options()
-# options.add_argument("--headless=new")      # New headless mode for recent Chrome
-# options.add_argument("--disable-gpu")       # Optional for stability
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from collections import Counter
 options = Options()
 options.add_argument("--headless=new")
-options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"')      
-passed = {}
-driver = webdriver.Chrome(options)
-username = "AnAverageElectrician"
-driver.get(f'https://codeforces.com/submissions/{username}/page/1')
-pageNumbers = driver.find_element(By.XPATH, '//*[@class="pagination"]')
-count_page_numbers = int(pageNumbers.text.split(" ")[-2])
-# print(count_page_numbers)
-for i in range(1,count_page_numbers+1):
-    driver.get(f'https://codeforces.com/submissions/{username}/page/{i}')
-    datatable_div  = driver.find_element(By.CLASS_NAME, "datatable")
-    links = datatable_div.find_elements(By.TAG_NAME, "a")
-    rows = datatable_div.find_elements(By.TAG_NAME, "tr")
-    for row in rows:
-        links = row.find_elements(By.TAG_NAME, "a")
-        if len(links)==0:
-            continue
-        if links[-1].get_attribute('href') not in passed and "Accepted" in row.text:
-                passed[links[-1].get_attribute('href')]=True
-                with open("links4.txt",'a') as f:
-                    f.write(f"{links[-1].get_attribute('href')}\n")
+options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"')
+
+def findLinks(linksFile,username,destinationpath)      
+    passed = {}
+    driver = webdriver.Chrome(options)
+    driver.get(f'https://codeforces.com/submissions/{username}/page/1')
+    pageNumbers = driver.find_element(By.XPATH, '//*[@class="pagination"]')
+    count_page_numbers = int(pageNumbers.text.split(" ")[-2])
+    # print(count_page_numbers)
+    for i in range(1,count_page_numbers+1):
+        driver.get(f'https://codeforces.com/submissions/{username}/page/{i}')
+        datatable_div  = driver.find_element(By.CLASS_NAME, "datatable")
+        links = datatable_div.find_elements(By.TAG_NAME, "a")
+        rows = datatable_div.find_elements(By.TAG_NAME, "tr")
+        for row in rows:
+            links = row.find_elements(By.TAG_NAME, "a")
+            if len(links)==0:
+                continue
+            if links[-1].get_attribute('href') not in passed and "Accepted" in row.text:
+                    passed[links[-1].get_attribute('href')]=True
+                    with open(linksFile+destinationpath,'a') as f:
+                        f.write(f"{links[-1].get_attribute('href')}\n")
